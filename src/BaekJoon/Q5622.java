5827f4c5a266817f269e36c458cac6f22f401a44
lukejihwan <luke9701@naver.com>
2023-12-01 23:43:29 +0900

    Q5622 문제 풀기

---
+++ src/BaekJoon/Q5622.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q5622 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
