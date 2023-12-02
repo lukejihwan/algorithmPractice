4ac7a90e95368f8c769003d8179feb726852b1fb
lukejihwan <luke9701@naver.com>
2023-12-02 23:55:51 +0900

    수정

--- src/BaekJoon/Q5622.java
+++ src/BaekJoon/Q5622.java
@@ -1,13 +1,15 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Q5622 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\n");
+
+        br.close();
+        bw.close();
     }
 }
