105469de6b58fd6a0a2ef53e4897a63e5fbfe920
lukejihwan <luke9701@naver.com>
2023-12-03 03:03:52 +0900

    5622번 문제풀기

--- src/BaekJoon/Q5622.java
+++ src/BaekJoon/Q5622.java
@@ -7,7 +7,58 @@ public class Q5622 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("\n");
+        String str = br.readLine();
+        char[] charArray = str.toCharArray();
+        int sec = 0;
+
+        for (char oneChar : charArray) {
+            switch (oneChar) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    sec += 3;
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    sec += 4;
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    sec += 5;
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    sec += 6;
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    sec += 7;
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    sec += 8;
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    sec += 9;
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    sec += 10;
+                    break;
+            }
+        }
+
+        bw.write(sec + "\n");
 
         br.close();
         bw.close();
