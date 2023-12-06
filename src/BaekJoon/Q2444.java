52f85d2d5f52a04a03ee3d3e95c87f336c01f467
lukejihwan <luke9701@naver.com>
2023-12-06 23:23:28 +0900

    2444문제 풀기 통과

--- src/BaekJoon/Q2444.java
+++ src/BaekJoon/Q2444.java
@@ -10,19 +10,17 @@ public class Q2444 {
         int repetition = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < repetition * 2 - 1; i++){
-            if(i < repetition){
-                sb.append(" ".repeat((repetition - i) - 1));
-                sb.append("*".repeat((i + 1) * 2 - 1 ) + "\n");
-            }
-
-            if(i >= repetition){
-                sb.append(" ".repeat((i - repetition) + 1));
-                sb.append("*".repeat((repetition * 2 - i) + (repetition - i) + 2) + "\n");
-            }
+        for(int i = 0; i < repetition; i++){
+            sb.append(" ".repeat((repetition - i) - 1));
+            sb.append("*".repeat((i + 1) * 2 - 1 ) + "\n");
         }
 
-        bw.write(sb.toString() + "\n");
+        for(int i = repetition - 1; i > 0; i--){
+            sb.append(" ".repeat(repetition - i));
+            sb.append("*".repeat(i * 2 - 1) + "\n");
+        }
+
+        bw.write(sb.toString());
         br.close();
         bw.close();
     }
