3b24e25033477ca33a631c6922b785d300c36217
lukejihwan <luke9701@naver.com>
2023-12-16 23:13:15 +0900

    1316번 문제 풀기

--- src/BaekJoon/Q1316.java
+++ src/BaekJoon/Q1316.java
@@ -10,24 +10,34 @@ public class Q1316 {
         // 단어의 갯수를 입력 받는다
         int repetition = Integer.parseInt(br.readLine());
         String[] strArr = new String[repetition];
+        boolean flag = true;
+        int count = 0;
 
         // 단어 갯수 만큼 단어를 입력 받는다.
         for(int i = 0; i < repetition; i++){
+            flag = true;
             strArr[i] = br.readLine();
 
             // 각 단어 별로 그룹 단어 인지 파악한다.
             for(int j = 0; j < strArr[i].length(); j++){
                 char a = strArr[i].charAt(j);
-                if()
+                int lastIndex = strArr[i].lastIndexOf(a);
+
+                for(int t = j; t <= lastIndex; t++){
+                    if(strArr[i].charAt(t) != a){
+                        flag = false;
+                    }
+                }
             }
 
-            /* 단어의 첫번째 char가 나머지 부분에 존재하는지 파악하고,
-            존재한다면 return 한다.
-            아니면, 두번째 char로 넘어가 나머지 부분에 존재하는지 파악한다.
-            이 과정을 단어끝까지 반복한다. 통과시 그룹단어로 인정된다.
-            * */
+            if(flag){
+                count++;
+            }
         }
 
-        // 그룹 단어의 갯수를 출력한다.
+        bw.write(count + "\n");
+        br.close();
+        bw.close();
+
     }
 }
