0af70e0df11c5792c2fd61d0deca2e356380eebc
lukejihwan <luke9701@naver.com>
2024-01-08 22:36:18 +0900

    Q25206 코드 수정

--- src/BaekJoon/Q25206.java
+++ src/BaekJoon/Q25206.java
@@ -4,11 +4,24 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
 
 public class Q25206 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashMap<String,Double> map = new HashMap<>();
+        map.put("A+", 4.5);
+        map.put("A0", 4.0);
+        map.put("B+", 3.5);
+        map.put("B0", 3.0);
+        map.put("C+", 2.5);
+        map.put("C0", 2.0);
+        map.put("D+", 1.5);
+        map.put("D0", 1.0);
+        map.put("F", 0.0);
+
 
 
     }
