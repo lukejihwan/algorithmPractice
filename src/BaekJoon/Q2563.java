551727cea82d19cc43f389d85135001d83170f6d
lukejihwan <luke9701@naver.com>
2024-01-16 00:16:01 +0900

    Q2563문제 수정

--- src/BaekJoon/Q2563.java
+++ src/BaekJoon/Q2563.java
@@ -8,33 +8,33 @@ public class Q2563 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //색종이 갯수를 입력 받는다.
+        //색종이 갯수를 입력받는다.
         int count = Integer.parseInt(br.readLine());
+        int total = 0;
 
-        //색종이 갯수 만큼을 곱해서 면적을 저장한다.
-        int area = count * 10 * 10;
+        //도화지 크기가 100, 100이므로, 0부터 100까지의 면적을 boolean으로 false로 만든다.
+        boolean[][] boolArr = new boolean[101][101];
 
-        //두번째 줄의 x값에 10을 더한값과 y값에 10을 더한 값과 그 아래 나오는 숫자들이
-        //둘다 작을 경우에만 곱해서 면적에 저장한다.
-        ArrayList<Pair> pairArr= new ArrayList<Pair>();
-
+        //for 문을 갯수만큼 돌면서, x와 y좌표를 추출하여 x부터 x+9까지, y부터 y+9까지 true로 만든다.
         for(int i = 0; i < count; i++){
             String[] strArr = br.readLine().split(" ");
-            pairArr.add(new Pair(Integer.parseInt(strArr[0]), Integer.parseInt(strArr[1])));
+            int x = Integer.parseInt(strArr[0]);
+            int y = Integer.parseInt(strArr[1]);
+
+            for(int j = x; j < x + 10; j++){
+                for(int z = y;  z < y + 10; z++ ){
+                    if(!boolArr[j][z]){
+                        boolArr[j][z] = true;
+                        total++;
+                    }
+                }
+            }
         }
 
-        System.out.println(pairArr.get(0).x);
-
-        //색종이 면적에서 곱해서 나온 면적을 뺸다.
+        //true의 갯수를 출력한다.
+        bw.write(total + "\n");
+        br.close();
+        bw.close();
     }
 
-
-}
-
-class Pair{
-    int x, y;
-    Pair(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
 }
