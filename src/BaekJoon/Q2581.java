37a6d9e73729aef709e2d4bf7c2993e0e5a86c43
lukejihwan <luke9701@naver.com>
2024-01-30 22:55:36 +0900

    Q2581 문제 풀기

---
+++ src/BaekJoon/Q2581.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import java.awt.image.BufferedImageFilter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2581 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+    }
+}
