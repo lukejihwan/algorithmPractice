841d9d6321014c2be45277e8bb1cb5f3da3b9a84
lukejihwan <luke9701@naver.com>
2024-02-01 21:55:08 +0900

    Q11653 문제풀기

---
+++ src/BaekJoon/Q11653.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Q11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        if(N != 1){
+            ArrayList<Integer> list = new ArrayList<>();
+            int divideNum = 2; //2부터 소수이다.
+
+            //구한 소수를 작은 수부터 나누어 나누어 떨어지면 출력한다
+            while(divideNum <= N){
+                int num = N % divideNum;
+                if(num == 0){
+                    N = N / divideNum;
+                    bw.write(divideNum + "\n");
+                }else{
+                    divideNum++;
+                }
+            }
+
+            br.close();
+            bw.close();
+        }
+    }
+}
