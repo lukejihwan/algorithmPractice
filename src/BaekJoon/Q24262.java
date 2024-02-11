370301b202afd86aa3ca2cbf630dcc7746a00bee
lukejihwan <luke9701@naver.com>
2024-02-11 23:25:09 +0900

    Q24262 문제풀기 성공

--- src/BaekJoon/Q24262.java
+++ src/BaekJoon/Q24262.java
@@ -1,17 +1,25 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Q24262 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        /*MenOfPassion(A[], n) {
+            i = ⌊n / 2⌋;
+            return A[i]; # 코드1
+        }*/
 
+        //위 식의 수행 횟수는 언제나 한 번이다.
 
+        int inputNum = Integer.parseInt(br.readLine());
+
+        bw.write(1 + "\n");
+        bw.write(0 + "");
+        br.close();
+        bw.close();
 
     }
 }
