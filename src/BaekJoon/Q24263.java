e982e1ee3b8d115db330b3a6a4aba04051d48fe0
lukejihwan <luke9701@naver.com>
2024-02-12 12:43:49 +0900

    Q24624 문제 풀기 빅오표기법 O(n)

---
+++ src/BaekJoon/Q24263.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.io.*;
+
+public class Q24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        bw.write(count + "\n");
+        bw.write(1 + "");
+        br.close();
+        bw.close();
+
+    }
+}
