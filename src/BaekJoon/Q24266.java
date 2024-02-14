5285f2e1c394b049055b18c9e09d2df11ebc0c2a
lukejihwan <luke9701@naver.com>
2024-02-14 20:42:40 +0900

    Q24266 문제 주석 추가

--- src/BaekJoon/Q24266.java
+++ src/BaekJoon/Q24266.java
@@ -7,6 +7,17 @@ public class Q24266 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        /*MenOfPassion(A[], n) {
+            sum <- 0;
+            for i <- 1 to n - 2
+                for j <- i + 1 to n - 1
+                    for k <- j + 1 to n
+                        sum <- sum + A[i] × A[j] × A[k]; # 코드1
+            return sum;
+        }
+        알고리즘 시간 복잡도 문제
+        * */
+
         long N = Integer.parseInt(br.readLine());
 
         bw.write(N * N * N + "\n");
