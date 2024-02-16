6db977d8d302d40450689362b0e222f67962705c
lukejihwan <luke9701@naver.com>
2024-02-16 22:29:55 +0900

    Q24267 문제 재해석

--- src/BaekJoon/Q24267.java
+++ src/BaekJoon/Q24267.java
@@ -10,13 +10,30 @@ public class Q24267 {
         int N = Integer.parseInt(br.readLine());
 
         // 3 -> 1, 1, 1 = 1
-        // 4 -> 2, 2!, 2! = 8
-        // 5 -> 3, 3!, 3! = 18
+        // 4 -> 2, 2!, 2! = 4 + 1 = 5
+        // 5 -> 3, 3!, 3! = 9 + 4 + 1 = 14
+        // 6 -> = 16 + 9 + 4 + 1 = 30
+        // 7 -> 5, 5, 5 = 25 + 16
         long rep = N * 5;
-        bw.write(rep + "\n");
-        bw.write(3 + "");
+        //bw.write(rep + "\n");
+        //bw.write(3 + "");
+
+        int num = MenOfPassion(7);
+        System.out.println(num);
 
         br.close();
         bw.close();
+    }
+
+    public static int MenOfPassion(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n - 2; i++) {
+            for (int j = i + 1; j <= n - 1; j++) {
+                for (int k = j + 1; k <= n; k++) {
+                    sum += 1;
+                }
+            }
+        }
+        return sum;
     }
 }
