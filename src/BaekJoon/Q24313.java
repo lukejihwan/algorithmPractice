bc3cb68cbc1d198fa349b9a5f619616740e573c5
lukejihwan <luke9701@naver.com>
2024-02-20 23:25:15 +0900

    Q24313 문제풀기

--- src/BaekJoon/Q24313.java
+++ src/BaekJoon/Q24313.java
@@ -1,13 +1,17 @@
 package BaekJoon;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Q24313 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int count = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int first = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
 
     }
 }
