b7e960e62dcf4161ec7ac318cc5423b2411c11ef
lukejihwan <luke9701@naver.com>
2024-02-21 21:05:20 +0900

    Q24313 문제 풀기 성공

--- src/BaekJoon/Q24313.java
+++ src/BaekJoon/Q24313.java
@@ -10,8 +10,20 @@ public class Q24313 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int first = Integer.parseInt(st.nextToken());
-        int second = Integer.parseInt(st.nextToken());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if (a1*n0 + a0 <= c*n0 && c >= a1) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        br.close();
+        bw.close();
 
     }
 }
