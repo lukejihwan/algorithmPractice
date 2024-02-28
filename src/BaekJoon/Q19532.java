6e539984ed8f101e4d21ed2f13976c6ccdcc1f2b
lukejihwan <luke9701@naver.com>
2024-02-28 23:26:29 +0900

    Q19532 문제 풀기

--- src/BaekJoon/Q19532.java
+++ src/BaekJoon/Q19532.java
@@ -17,9 +17,18 @@ public class Q19532 {
         int e = Integer.parseInt(st.nextToken());
         int f = Integer.parseInt(st.nextToken());
 
+        //두개의 연립 방정식을 모두 더한다.
 
+        //x에 -999 부터 999까지 대입하면서 y가 정수로 출력되는 경우
 
+        //x와 y를 출력한다.
+        // 두 방정식을 더한 후, x 를 구합니다.
+        int x = (c * e - b * f) / (a * e - b * d);
+        // 두 방정식 중 하나를 선택하여 y 를 구합니다.
+        int y = (c - a * x) / b;
 
-
+        bw.write(x + " " + y + "\n");
+        br.close();
+        bw.close();
     }
 }
