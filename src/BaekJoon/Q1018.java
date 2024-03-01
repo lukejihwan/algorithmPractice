ab64ba64da847843fd2530d3feca4f37af2be6cb
lukejihwan <luke9701@naver.com>
2024-03-01 23:48:01 +0900

    Q1018 문제 풀기

--- src/BaekJoon/Q1018.java
+++ src/BaekJoon/Q1018.java
@@ -1,15 +1,13 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Q1018 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int a = Integer.parseInt(br.readLine());
 
 
     }
