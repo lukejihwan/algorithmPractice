a94b6bd310b06bf8408779ce74c01b49377150bf
lukejihwan <luke9701@naver.com>
2024-03-04 23:57:40 +0900

    Q1018_ver2 다른방식으로 문제 풀기

--- src/BaekJoon/Q1018.java
+++ src/BaekJoon/Q1018.java
@@ -1,13 +1,19 @@
 package BaekJoon;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Q1018 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        //배열에 모두 담는다.
 
 
     }
