38d8f14a25216d3a7f638ca9924abc1ba140baab
lukejihwan <luke9701@naver.com>
2024-03-09 23:56:43 +0900

    Q11724번문제 DFS문제 다시 풀어보기

---
+++ src/BaekJoon/Q11724.java
@@ -0,0 +1,60 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q11724 {
+    static boolean visited[];
+    static ArrayList<Integer>[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 1. 첫번째 줄의 첫째는 정점의 갯수를 말하고, 첫번째 줄의 둘째는 간선의 갯수를 나타낸다.
+
+        // 2.
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1]; // 0번과 헷갈리지 않도록
+        A = new ArrayList[N + 1];
+
+        for(int i = 1; i < N + 1; i++){
+            A[i] = new ArrayList<Integer>();
+        }
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                count++;
+                DFS(i);
+            }
+        }
+        bw.write(count + "\n");
+        br.close();
+        bw.close();
+
+    }
+
+    private static void DFS(int v) {
+        if(visited[v]){
+            return;
+        }
+        visited[v] = true;
+        for(int i : A[v]){
+            if(!visited[i]){
+                DFS(i);
+            }
+        }
+    }
+}
