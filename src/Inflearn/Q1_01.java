bb88482080349c2e2829f5ca7f3be4c5d0c55c99
lukejihwan <luke9701@naver.com>
2024-04-03 23:46:39 +0900

    Q1_01 여러방식 문제 풀기

--- src/Inflearn/Q1_01.java
+++ src/Inflearn/Q1_01.java
@@ -1,11 +1,25 @@
 package Inflearn;
 
 import java.util.Scanner;
-import java.util.*;
 import java.io.*;
 
 public class Q1_01 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        char one = in.next().charAt(0);
+
+        str = str.toUpperCase();
+        one = Character.toUpperCase(one);
+        int count = 0;
+        for(char result : str.toCharArray()){
+            if(one == result) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private int resolve1() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] charArr = br.readLine().toUpperCase().toCharArray();
         String oneChar = br.readLine();
@@ -14,7 +28,21 @@ public class Q1_01 {
         for(int i = 0; i < charArr.length; i++){
             if(String.valueOf(charArr[i]).equals(oneChar.toUpperCase())) count++;
         }
-
-        System.out.println(count);
+        return count;
     }
+
+    private int resolve2(){
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+        char one = in.next().charAt(0);
+
+        str = str.toUpperCase();
+        one = Character.toUpperCase(one);
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(one == str.charAt(i)) count ++;
+        }
+        return count;
+    }
+
 }
