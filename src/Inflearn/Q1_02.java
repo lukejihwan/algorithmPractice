f5d54017a4309e60a7bdac22a5eb6b1e9ad51536
lukejihwan <luke9701@naver.com>
2024-04-04 23:16:39 +0900

    Q1_02 문제 다른 방식으로 푸는 법 추가

--- src/Inflearn/Q1_02.java
+++ src/Inflearn/Q1_02.java
@@ -3,6 +3,31 @@ package Inflearn;
 import java.util.Scanner;
 public class Q1_02 {
     public static void main(String[] args) {
+        /*
+        * ASCII코드표에서 영문 소문자는 97 ~ 122
+        * 대문자는 65 ~ 90이다.
+        * */
+
+        String str = new Q1_02().solution1();
+
+
+        System.out.println(str);
+    }
+
+    //Character Wrapper클래스를 사용한 방식
+    private String solution2(){
+        String answer = "";
+        Scanner sc = new Scanner(System.in);
+
+        for(char N : sc.next().toCharArray()){
+            if(Character.isLowerCase(N)) answer += Character.toUpperCase(N);
+            else answer += Character.toLowerCase(N);
+        }
+
+        return answer;
+    }
+
+    private String solution1(){
         Scanner in = new Scanner(System.in);
         String str = in.next();
         StringBuilder sb = new StringBuilder();
@@ -14,6 +39,7 @@ public class Q1_02 {
             }
         }
 
-        System.out.println(sb.toString());
+        return sb.toString();
     }
+
 }
