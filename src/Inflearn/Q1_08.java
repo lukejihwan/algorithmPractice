154a7709f625b84888ea9380621e7605e3ac45af
lukejihwan <luke9701@naver.com>
2024-04-13 23:12:18 +0900

    Q1_08 다른 방식으로 풀기 및 배운내용 추가

--- src/Inflearn/Q1_08.java
+++ src/Inflearn/Q1_08.java
@@ -3,11 +3,18 @@ package Inflearn;
 import java.util.Scanner;
 
 public class Q1_08 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine().toUpperCase();
-        char[] charArr = str.toCharArray();
+
+    private String solution1(String str){
+        String answer = "NO";
+        char[] charArr = str.toUpperCase().toCharArray();
         int lt = 0, rt = str.length() - 1;
+        /*
+         * 배운 내용 : next() 메서드는 delimeter를 이전 문자열만 가져오게 된다.
+         * 즉, "asd ads da" 문자열에서 asd까지만 next() 문자열로 하나의 토큰으로 인식
+         * 한다는 것이다.
+         * 반면에 nextLine() 메서드는 \n 이나 \r같은 delimeter까지 가져오게된다.
+         * 그래서 "asdf asd df" 문자열에 공백이나 개행문자까지 하나의 문자열로 가져오는 것이다.
+         * */
 
         while(lt < rt){
             if(!Character.isAlphabetic(charArr[lt])){
@@ -16,15 +23,32 @@ public class Q1_08 {
                 rt--;
             }else{
                 if(charArr[lt] != charArr[rt]){
-                    System.out.println("NO");
-                    return;
+                    return answer;
                 }
                 lt++;
                 rt--;
             }
         }
+        answer = "YES";
 
-        System.out.println("YES");
-        return ;
+        return answer;
+    }
+
+    private String solution2(String str){
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String reversedStr = new StringBuilder(str).reverse().toString();
+
+        if(str.equals(reversedStr)) return answer = "YES";
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        Q1_08 object = new Q1_08();
+        System.out.println(object.solution1(str));
+        System.out.println(object.solution2(str));
     }
 }
