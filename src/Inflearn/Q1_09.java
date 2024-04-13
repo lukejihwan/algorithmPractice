e9481debf6854b57f19f18899f93c9b6f55f1a3e
lukejihwan <luke9701@naver.com>
2024-04-14 08:48:55 +0900

    Q1_09 숫자추출문제 다른 방식으로 풀기

--- src/Inflearn/Q1_09.java
+++ src/Inflearn/Q1_09.java
@@ -3,9 +3,8 @@ package Inflearn;
 import java.util.Scanner;
 
 public class Q1_09 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
+
+    protected int solution1(String str){
         String answer = "";
         char[] charArr = str.toCharArray();
 
@@ -13,7 +12,25 @@ public class Q1_09 {
             if(Character.isDigit(N)) answer += N;
         }
 
-        System.out.println(Integer.parseInt(answer));
+        return Integer.parseInt(answer);
+    }
+
+    protected int solution2(String str){
+        int answer = 0;
+
+        for(char N : str.toCharArray()){
+            if(N >= 48 && N <= 57) answer = answer * 10 + (N - 48);
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        Q1_09 object = new Q1_09();
+        System.out.println(object.solution1(str));
+        System.out.println(object.solution2(str));
         return ;
     }
 }
