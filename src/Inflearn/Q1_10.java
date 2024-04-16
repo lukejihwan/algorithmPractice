c16d64f83fe139e25295066908bcc3df6996a13e
lukejihwan <luke9701@naver.com>
2024-04-16 23:49:22 +0900

    Q1_10 다른 방식으로 풀기 임시저장

--- src/Inflearn/Q1_10.java
+++ src/Inflearn/Q1_10.java
@@ -12,11 +12,28 @@ public class Q1_10 {
 
         int[] indexArr = new int[str.length()];
 
+        int count = 0;
+        String answer = "";
         for(int i = 0; i < charArr.length; i++){
             if(charArr[i] == N){
                 indexArr[i] = 0;
+                count = 0;
+            }else{
+                count++;
+                indexArr[i] = count;
             }
+
+            /*if(i == charArr.length - 1){
+                answer += indexArr[i];
+            }else{
+                answer += indexArr[i] + " ";
+            }*/
         }
+
+        for(int i = charArr.length - 1; i >= 0; i--){
+        }
+
+        System.out.println(answer);
 
     }
 }
