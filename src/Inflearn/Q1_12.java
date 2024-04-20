769ec67fe6a18363dc3c14a8cb6279457607c48d
lukejihwan <luke9701@naver.com>
2024-04-20 23:37:31 +0900

    Q1_12 문제 풀기

---
+++ src/Inflearn/Q1_12.java
@@ -0,0 +1,11 @@
+package Inflearn;
+
+import java.util.Scanner;
+
+public class Q1_12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+    }
+}
