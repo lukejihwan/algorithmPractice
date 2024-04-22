f1503d25d72411fa790e9f389acee7a643ff432b
lukejihwan <luke9701@naver.com>
2024-04-22 23:06:22 +0900

    Q2_01 배열 문제 큰 수 출력하기 다른 방식으로 풀기

--- src/Inflearn/Q2_01.java
+++ src/Inflearn/Q2_01.java
@@ -1,8 +1,33 @@
 package Inflearn;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q2_01 {
+
+    private static ArrayList<Integer> solution1(int[] intArr){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i = 0; i < intArr.length; i++){
+            if(i != 0 && intArr[i - 1] < intArr[i]){
+                list.add(intArr[i]);
+            }else if(i == 0){
+                list.add(intArr[i]);
+            }
+        }
+
+        return list;
+    }
+
+    private static ArrayList<Integer> solution2(int[] intArr){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(intArr[0]);
+        for(int i = 1; i < intArr.length; i++){
+            if(intArr[i - 1] < intArr[i]) list.add(intArr[i]);
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
@@ -10,12 +35,14 @@ public class Q2_01 {
         for(int i = 0; i < N; i++){
             intArr[i] = in.nextInt();
         }
-        for(int i = 0; i < N; i++){
-            if(i != 0 && intArr[i - 1] < intArr[i]){
-                System.out.print(intArr[i] + " ");
-            }else if(i == 0){
-                System.out.print(intArr[i] + " ");
-            }
+        for(int num : solution1(intArr)){
+            System.out.print(num + " ");
         }
+        System.out.println();
+        for(int num : solution2(intArr)){
+            System.out.print(num + " ");
+        }
+
+
     }
 }
