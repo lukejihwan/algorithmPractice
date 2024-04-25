e83ab336d9ef189507193e68a9520dc8dd260a5c
lukejihwan <luke9701@naver.com>
2024-04-26 00:01:45 +0900

    Q2_04 피보나치 수열 문제 다른 방식으로 풀기

--- src/Inflearn/Q2_04.java
+++ src/Inflearn/Q2_04.java
@@ -3,10 +3,8 @@ package Inflearn;
 import java.util.Scanner;
 
 public class Q2_04 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
 
+    private static int[] solution1(int N){
         // 최소 입력 수가 3 이므로, 3까지 입력하고 그 다음부터 for-loop을 돈다.
         // 앞의 두 수의 합을 N까지 출력해 나간다.
         int[] Arr = new int[N];
@@ -15,8 +13,27 @@ public class Q2_04 {
         for(int i = 2; i < N; i++){
             Arr[i] = Arr[i - 1] + Arr[i - 2];
         }
-        for(int K : Arr){
+        return Arr;
+    }
+    private static void solution2(int N){
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for(int i = 2; i < N; i++){
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int K : solution1(N)){
             System.out.print(K + " ");
         }
+        System.out.println();
+        solution2(N);
     }
 }
