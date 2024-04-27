3b1356f782afaa89db862706d60017280a2ab203
lukejihwan <luke9701@naver.com>
2024-04-27 16:07:18 +0900

    Q2_05 리팩토링

--- src/Inflearn/Q2_05.java
+++ src/Inflearn/Q2_05.java
@@ -3,13 +3,11 @@ package Inflearn;
 import java.util.Scanner;
 
 public class Q2_05 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    private static int solution1(int N){
         // 숫자 N을 입력받는다.
         // 2부터 N까지 반복하면서 for-loop을 도는 숫자가 소수인지 체크하고 해당 수부터 N까지의 곱에 해당하는
         // 숫자들은 반복문을 돌지 않도록한다.
         // 3도 마찬가지로 위와 같이 실행한다.
-        int N = in.nextInt();
         int count = 0;
         int[] Arr = new int[N + 1];
         for(int i = 2; i <= N; i++){
@@ -17,6 +15,13 @@ public class Q2_05 {
             for(int j = i; j <= N; j = j + i) Arr[j] = 1;
         }
 
-        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        System.out.println(solution1(N));
     }
 }
