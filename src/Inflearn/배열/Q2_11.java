0f45a5ab77caf6627e621ee7fad03d83d5999ef0
lukejihwan <luke9701@naver.com>
2024-05-02 22:00:05 +0900

    Q2_11 임시 반장 정하기 다른 방식으로 문제 풀기

--- src/Inflearn/배열/Q2_11.java
+++ src/Inflearn/배열/Q2_11.java
@@ -3,19 +3,8 @@ package Inflearn.배열;
 import java.util.Scanner;
 
 public class Q2_11 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
 
-        // 2차배열에 각 학생별 반 수를 저장한다.
-        // N 번을 반복하면서 각 학생수와 같은 반을 한 친구 수를 비교하여, 가장 많은 학생을 출력한다.
-        int[][] arr = new int[5][N];
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < N; j++){
-                arr[i][j] = in.nextInt();
-            }
-        }
-
+    private static int solution1(int[][] arr, int N){
         int previous = 0, count = 0, monitor = 0;
         for(int a = 0; a < N; a++){
             boolean[] isSame = new boolean[N];
@@ -37,6 +26,44 @@ public class Q2_11 {
             }
         }
 
-        System.out.println(monitor + 1);
+        return monitor + 1;
+    }
+
+    private static int solution2(int[][] arr, int N){
+        int max = Integer.MIN_VALUE, answer = 0;
+        for(int i = 0; i < N; i++){
+            int count = 0;
+            for(int j = 0; j < N; j++){
+                for(int a = 0; a < 5; a++){
+                    if(arr[i][a] == arr[j][a]){
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if(max < count){
+                max = count;
+                answer =  i + 1;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        // 2차배열에 각 학생별 반 수를 저장한다.
+        // N 번을 반복하면서 각 학생수와 같은 반을 한 친구 수를 비교하여, 가장 많은 학생을 출력한다.
+        int[][] arr = new int[N][5];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < 5; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println(solution1(arr, N));
+        System.out.println(solution2(arr, N));
     }
 }
