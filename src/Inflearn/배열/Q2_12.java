737e59bb127efb562b47d139c38f6c0fcfad8e08
lukejihwan <luke9701@naver.com>
2024-05-03 23:54:51 +0900

    Q2_12 멘토링 문제 풀기

---
+++ src/Inflearn/배열/Q2_12.java
@@ -0,0 +1,12 @@
+package Inflearn.배열;
+
+import java.util.Scanner;
+
+public class Q2_12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+    }
+}
