d0991c8771efec4616e9362e456e8429a272153d
lukejihwan <luke9701@naver.com>
2024-05-06 15:10:14 +0900

    Q3_01 다른 방식으로 문제 풀기

--- src/Inflearn/포인터/Q3_01.java
+++ src/Inflearn/포인터/Q3_01.java
@@ -1,8 +1,56 @@
 package Inflearn.포인터;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q3_01 {
+
+    private static ArrayList<Integer> solution1(int[] arrN, int[] arrM, int N, int M)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int index1 = 0;
+        int index2 = 0;
+        for(int i = 0; i < M + N; i++){
+            int num1 = arrN[index1];
+            int num2 = arrM[index2];
+            if(num1 < num2 && index1 < N){
+                list.add(num1);
+                index1++;
+            } else if (num1 >= num2 && index2 < M) {
+                list.add(num2);
+                index2++;
+            }
+        }
+
+        return list;
+    }
+
+    private static ArrayList<Integer> solution2(int[] arrN, int[] arrM, int N, int M)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int pn = 0;
+        int pm = 0;
+        while (pn < N && pm < M){
+            if(arrN[pn] < arrM[pm]){
+                list.add(arrN[pn]);
+                pn++;
+            }else {
+                list.add(arrM[pm]);
+                pm++;
+            }
+        }
+        while (pn < N){
+            list.add(arrN[pn]);
+            pn++;
+        }
+        while (pm < M){
+            list.add(arrM[pm]);
+            pm++;
+        }
+        return list;
+    }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
@@ -13,29 +61,22 @@ public class Q3_01 {
         // 3과 2를 비교해서, 2를 출력하고 index2++한다. 같은 경우에도 둘중하나를 ++을 하고 계속 진행한다.
 
         int N = in.nextInt();
-        int[] arrN = new int[N];
+        int[] arrN = new int[N + 1];
         for(int i = 0; i < N; i++){
             arrN[i] = in.nextInt();
         }
         int M = in.nextInt();
-        int[] arrM = new int[M];
+        int[] arrM = new int[M + 1];
         for(int i = 0; i < M; i++){
             arrM[i] = in.nextInt();
         }
 
-        int index1 = 0;
-        int index2 = 0;
-        for(int i = 0; i < M + N; i++){
-            int num1 = arrN[index1];
-            int num2 = arrM[index2];
-            if(num1 < num2 && index1 < N){
-                System.out.print(num1 + " ");
-                index1++;
-            } else if (index2 < M) {
-                System.out.print(num2 + " ");
-                index2++;
-            }
+        for(int res : solution1(arrN, arrM, N, M)){
+            System.out.print(res + " ");
         }
-        return ;
+        System.out.println();
+        for(int res : solution2(arrN, arrM, N, M)){
+            System.out.print(res + " ");
+        }
     }
 }
