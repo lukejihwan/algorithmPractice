1bfe9c339d7179a9c09d75be1b948ebd584825cc
lukejihwan <luke9701@naver.com>
2024-05-08 00:14:55 +0900

    Q3_03 코드 리팩토링 후 재 커밋

--- src/Inflearn/포인터/Q3_03.java
+++ src/Inflearn/포인터/Q3_03.java
@@ -14,14 +14,15 @@ public class Q3_03 {
             arr[i] = in.nextInt();
         }
 
-        int max = 0;
-        int index = 0;
-        while(index < N - K + 1){
-            int sum = 0;
-            for(int i = index; i < index + K; i++){
-                sum += arr[i];
-            }
-            if(max < sum)max = sum;
+        int max = 0, sum, index=0;
+        for(int i = 0; i < K; i++) max += arr[i];
+        sum = max;
+
+        // 계속 더하게 되면 time limit을 초과하게 되므로, 원래 더했던 숫자에 처음값만 빼고 맨 뒤에 값만 더하는 식으로 하면
+        // 매번 더할 필요가 없게 된다.
+        while(index < N - K){
+            sum += (arr[index + K] - arr[index]);
+            max = Math.max(max, sum);
             index++;
         }
 
