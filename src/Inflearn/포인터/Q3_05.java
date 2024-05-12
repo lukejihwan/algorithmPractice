c6c2d8d8f9ad35009f23f9844b1f04028e210673
lukejihwan <luke9701@naver.com>
2024-05-12 11:20:11 +0900

    Q3_05 연속된 자연수 합 문제 수정

--- src/Inflearn/포인터/Q3_05.java
+++ src/Inflearn/포인터/Q3_05.java
@@ -22,7 +22,7 @@ public class Q3_05 {
     }
     private static int solution2(int[] arr, int N){
         int sum = 0, pf = 0, count = 0;
-        for(int pe = 0; pe < N - 1; pe++){
+        for(int pe = 0; pe < arr.length; pe++){
             sum += arr[pe];
             if(sum == N)count++;
             while(sum >= N){
@@ -33,6 +33,23 @@ public class Q3_05 {
 
         return count;
     }
+
+    //수학적 방법으로 풀기
+    private static int solution3(int N){
+        // 방법 : 2개 연속된 숫자일 때, N에서 1, 2를 빼고 난 나머지가 12라고 하면,
+        // 12 % 2 로 나누어 떨어지면 count++ 하는 방식
+
+        int answer = 0, count = 1;
+        N--;
+        while(N > 0){
+            count++;
+            N -= count;
+            if(N % count == 0) answer++;
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
@@ -53,6 +70,7 @@ public class Q3_05 {
         }
 
         System.out.println(solution1(arr, N));
-        System.out.println(solution2(arr2, M));
+        System.out.println(solution2(arr2, N));
+        System.out.println(solution3(N));
     }
 }
