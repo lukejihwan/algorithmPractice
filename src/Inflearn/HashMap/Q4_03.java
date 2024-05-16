84803911b6f2fa2859665a13c222d12f92f8cd96
lukejihwan <luke9701@naver.com>
2024-05-16 23:54:08 +0900

    Q4_03 매출액의 종류 문제 풀기

---
+++ src/Inflearn/HashMap/Q4_03.java
@@ -0,0 +1,22 @@
+package Inflearn.HashMap;
+
+import java.util.Scanner;
+
+public class Q4_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for(int i = 0; i < N - K + 1; i++){
+
+        }
+
+
+    }
+}
