b19e621c3824074eb8fb509711907d53a33cb9fe
lukejihwan <luke9701@naver.com>
2024-05-18 13:28:35 +0900

    Q4_04 모든 아나그램 찾기 문제 다른 방식으로 문제 풀기

--- src/Inflearn/HashMap/Q4_04.java
+++ src/Inflearn/HashMap/Q4_04.java
@@ -4,11 +4,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Q4_04 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str1 = in.next();
-        String str2 = in.next();
-
+    private static int solution1(String str1, String str2){
         HashMap<Character, Integer> hashMap2 = new HashMap<>();
 
         for(char key : str2.toCharArray()){
@@ -34,6 +30,39 @@ public class Q4_04 {
             rt++;
         }
 
-        System.out.println(answer);
+        return answer;
+    }
+
+    private static int solution2(String str1, String str2){
+        int answer = 0;
+
+        HashMap<Character, Integer> hashMap1 = new HashMap<>();
+        HashMap<Character, Integer> hashMap2 = new HashMap<>();
+
+        for(char key : str2.toCharArray()) hashMap2.put(key, hashMap2.getOrDefault(key, 0) + 1);
+
+        for(int i = 0; i < str2.length() - 1; i++){
+            hashMap1.put(str1.charAt(i), hashMap1.getOrDefault(str1.charAt(i), 0) + 1);
+        }
+
+        int lt = 0;
+        for(int rt = str2.length() - 1; rt < str1.length(); rt++){
+            hashMap1.put(str1.charAt(rt), hashMap1.getOrDefault(str1.charAt(rt), 0) + 1);
+            if(hashMap1.equals(hashMap2)) answer++;
+            hashMap1.put(str1.charAt(lt), hashMap1.get(str1.charAt(lt)) - 1);
+            if(hashMap1.get(str1.charAt(lt)) == 0) hashMap1.remove(str1.charAt(lt));
+            lt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str1 = in.next();
+        String str2 = in.next();
+
+        System.out.println(solution1(str1, str2));
+        System.out.println(solution2(str1, str2));
     }
 }
