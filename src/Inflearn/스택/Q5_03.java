ea3d829d788f683adbf7033f7a896dc312266919
lukejihwan <luke9701@naver.com>
2024-05-21 00:07:17 +0900

    Q5_03 크레인 인형뽑기 문제 풀기

--- src/Inflearn/스택/Q5_03.java
+++ src/Inflearn/스택/Q5_03.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 public class Q5_03 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int[][] board = new int[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                board[i][j] = in.nextInt();
+            }
+        }
+
+        int K = in.nextInt();
+        int[] moves = new int[K];
+        for(int i = 0; i < K; i++){
+            moves[i] = in.nextInt();
+        }
     }
 }
