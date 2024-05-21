cee8bf1dec60c082032c6f5f8094c3be0c70f164
lukejihwan <luke9701@naver.com>
2024-05-21 23:33:09 +0900

    Q5_03 크레인 인형뽑기 문제풀기

--- src/Inflearn/스택/Q5_03.java
+++ src/Inflearn/스택/Q5_03.java
@@ -1,5 +1,6 @@
 package Inflearn.스택;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q5_03 {
@@ -8,9 +9,9 @@ public class Q5_03 {
 
         int N = in.nextInt();
 
-        int[][] board = new int[N][N];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
+        int[][] board = new int[N + 1][N + 1];
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < N; j++){
                 board[i][j] = in.nextInt();
             }
         }
@@ -20,5 +21,40 @@ public class Q5_03 {
         for(int i = 0; i < K; i++){
             moves[i] = in.nextInt();
         }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < K; i++){
+            int index = 1;
+            while(board[moves[i]][index++] == 0){
+
+                int doll = board[moves[i]][index];
+                if(doll != 0){
+                    list.add(doll);
+                    board[moves[i]][index] = 0;
+                }
+
+            }
+        }
+
+        int answer = 0;
+
+        int lt = 0;
+        for(int rt = 1; rt < list.size(); rt++){
+            if(list.get(rt) == list.get(lt)){
+                answer += 2;
+                rt++;
+                lt--;
+                while(list.get(rt) == list.get(lt)){
+                    answer += 2;
+                    rt++;
+                    lt--;
+                }
+                lt = rt - 2;
+            }
+            lt++;
+        }
+
+        System.out.println(answer);
+        return ;
     }
 }
