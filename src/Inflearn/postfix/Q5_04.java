91ca3ddb371334219dd56ebb6774b8481fa374fd
lukejihwan <luke9701@naver.com>
2024-05-23 23:13:59 +0900

    postfix문제 풀기

---
+++ src/Inflearn/postfix/Q5_04.java
@@ -0,0 +1,11 @@
+package Inflearn.postfix;
+
+import java.util.Scanner;
+
+public class Q5_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+    }
+}
