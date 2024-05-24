4a9a79ba4736a96f2f6653ccc5ebff54129474d8
lukejihwan <luke9701@naver.com>
2024-05-24 22:41:22 +0900

    Q5_04 다른 방식으로 문제 풀기

--- src/Inflearn/postfix/Q5_04.java
+++ src/Inflearn/postfix/Q5_04.java
@@ -4,16 +4,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Q5_04 {
-    public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        String str = in.next();
-
-        // 배열에 넣는다.
-        char[] arr = new char[str.length()];
-        for(int i = 0; i < str.length(); i++){
-            arr[i] = str.charAt(i);
-        }
-
+    private static int solution1(char[] arr){
         Stack<Integer> stack = new Stack<>();
 
         // Stack에 쌓는데, 기호가 나오면 두개를 빼서 계산하고 그 자리에 쌓는다.
@@ -36,6 +27,38 @@ public class Q5_04 {
             }
         }
 
-        System.out.println(stack.pop());
+        return stack.get(0);
+    }
+
+    private static int solution2(char[] arr){
+        Stack<Integer> stack = new Stack<>();
+
+        for(char n : arr){
+            if(Character.isDigit(n)){
+                stack.push(n - 48);
+            }else{
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if(n == '+') stack.push(lt + rt);
+                else if(n == '-') stack.push(lt - rt);
+                else if(n == '*') stack.push(lt * rt);
+                else if(n == '/') stack.push(lt / rt);
+            }
+        }
+
+        return stack.get(0);
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str = in.next();
+
+        // 배열에 넣는다.
+        char[] arr = new char[str.length()];
+        for(int i = 0; i < str.length(); i++){
+            arr[i] = str.charAt(i);
+        }
+
+        System.out.println(solution1(arr));
+        System.out.println(solution2(arr));
     }
 }
