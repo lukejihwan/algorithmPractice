79752a428314a15a4920cf3e355180d589fdf9a5
lukejihwan <luke9701@naver.com>
2024-05-25 23:01:44 +0900

    Q5_06 공주 구하기 다른 방식으로 문제 풀기

--- src/Inflearn/큐/Q5_06.java
+++ src/Inflearn/큐/Q5_06.java
@@ -5,11 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Q5_06 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int K = in.nextInt();
-
+    private static int solution1(int N, int K){
         // 큐를 사용해 구현한다.
         // 큐에 1부터 N까지 담는다.
         // K 전까지 큐 front에서 제거하고 뒤쪽으로 넣는다.
@@ -26,8 +22,30 @@ public class Q5_06 {
             queue.poll();
         }
 
-        System.out.println(queue.poll());
-        return ;
+        return queue.poll();
     }
+
+    private static int solution2(int N, int K){
+
+        Queue<Integer> Q = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++) Q.offer(i);
+
+        while(!Q.isEmpty()){
+            for(int i = 0; i < K - 1; i++) Q.offer(Q.poll());
+            Q.poll();
+            if(Q.size() == 1) return Q.poll();
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+
+        System.out.println(solution1(N, K));
+        System.out.println(solution2(N, K));
     }
 }
