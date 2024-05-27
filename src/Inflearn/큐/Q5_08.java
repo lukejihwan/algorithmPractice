fb81bb0224bdd373326c8f50d76d474bbc7c362c
lukejihwan <luke9701@naver.com>
2024-05-27 23:10:18 +0900

    다른 방식으로 문제 풀기

--- src/Inflearn/큐/Q5_08.java
+++ src/Inflearn/큐/Q5_08.java
@@ -5,19 +5,8 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Q5_08 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        int N = in.nextInt();
-        int K = in.nextInt();
-
-        // K번째 환자의 위험도보다 큰 환자들은 모두 앞으로 밀어 넣고,
-        // K번째 환자와 위험도가 같은 환자들의 순서를 기억해 두어야 한다.
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++){
-            arr[i] = in.nextInt();
-        }
-
+    private static int solution1(int[] arr, int N, int K){
         Queue<Person> q = new LinkedList<>();
         for(int i = 0; i < N; i++){
             q.offer(new Person(i, arr[i]));
@@ -40,11 +29,53 @@ public class Q5_08 {
             }
 
             if(flag && person.id == K){
-                System.out.println(answer);
-                break;
+                return answer;
             }
 
         }
+    }
+
+    private static int solution2(int[] arr, int N, int K){
+        Queue<Person> queue = new LinkedList<>();
+        for(int i = 0; i < N; i++){
+            queue.offer(new Person(i, arr[i]));
+        }
+        int answer = 0;
+        while(!queue.isEmpty()){
+            Person person = queue.poll();
+            for(Person x : queue){
+                if(person.priority < x.priority){
+                    queue.offer(person);
+                    person = null;
+                    break;
+                }
+            }
+
+            if(person != null){
+                answer++;
+                if(person.id == K) return answer;
+            }
+
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int K = in.nextInt();
+
+        // K번째 환자의 위험도보다 큰 환자들은 모두 앞으로 밀어 넣고,
+        // K번째 환자와 위험도가 같은 환자들의 순서를 기억해 두어야 한다.
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution1(arr, N, K));
+        System.out.println(solution2(arr, N, K));
+
     }
 }
 
