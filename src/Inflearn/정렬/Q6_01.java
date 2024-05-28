734be919adc3377860cd988f6230e5149b850be8
lukejihwan <luke9701@naver.com>
2024-05-28 23:49:11 +0900

    다른 방식으로 문제 풀기

--- src/Inflearn/정렬/Q6_01.java
+++ src/Inflearn/정렬/Q6_01.java
@@ -3,14 +3,7 @@ package Inflearn.정렬;
 import java.util.Scanner;
 
 public class Q6_01 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++){
-            arr[i] = in.nextInt();
-        }
-
+    private static int[] solution1(int[] arr, int N){
         for(int i = 0; i < N - 1; i++){
             int goal = arr[i];
             int min = Integer.MAX_VALUE;
@@ -27,7 +20,39 @@ public class Q6_01 {
             }
         }
 
-        for(int K : arr){
+        return arr;
+    }
+
+    private static int[] solution2(int[] arr, int N){
+        for(int i = 0; i < N - 1; i++){
+            int idx = i;
+            for(int j = i + 1; j < N; j++){
+                if(arr[idx] > arr[j]) idx = j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[] arr = new int[N];
+        int[] arr2 = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+            arr2[i] = arr[i];
+        }
+
+        for(int answer : solution1(arr, N)){
+            System.out.print(answer + " ");
+        }
+
+        System.out.println();
+
+        for(int K : solution2(arr2, N)){
             System.out.print(K + " ");
         }
     }
