90fcbd6633e7c47843c124adfc8794d34314ec82
lukejihwan <luke9701@naver.com>
2024-05-31 21:40:58 +0900

    버블정렬 문제 풀기

--- src/Inflearn/정렬/Q6_02.java
+++ src/Inflearn/정렬/Q6_02.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class Q6_02 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for(int i = 0; i < N - 1; i++){
+            for(int j = 0; j < N - i - 1; j++){
+                int num = arr[j];
+                if(num > arr[j + 1]){
+                    int temp = arr[j + 1];
+                    arr[j + 1] = num;
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int answer : arr){
+            System.out.print(answer + " ");
+        }
+
+        return ;
     }
 }
