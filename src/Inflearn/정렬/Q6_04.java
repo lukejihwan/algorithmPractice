04a0456f45d45e9da2681a616949ea6b5989eb1f
lukejihwan <luke9701@naver.com>
2024-06-02 00:48:03 +0900

    다른 방식으로 문제 풀기

--- src/Inflearn/정렬/Q6_04.java
+++ src/Inflearn/정렬/Q6_04.java
@@ -3,17 +3,8 @@ package Inflearn.정렬;
 import java.util.Scanner;
 
 public class Q6_04 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        int ea = in.nextInt();
-
-        int[] arr = new int[ea];
-        for(int i = 0; i < ea; i++){
-            arr[i] = in.nextInt();
-        }
-
-        int[] status = new int[size];
+    private static int[] solution1(int N, int K, int[] arr){
+        int[] status = new int[N];
         for(int key : arr){
             boolean flag = false;
             int index = -1;
@@ -37,7 +28,46 @@ public class Q6_04 {
             }
         }
 
-        for(int answer : status) System.out.print(answer + " ");
+        return status;
+    }
+
+    private static int[] solution2(int N, int K, int[] arr){
+        int[] cache = new int[N];
+
+        for(int num : arr){
+            int pos = -1;
+            for(int i = cache.length - 1; i >= 1; i--){
+                if(num == cache[i]) pos = i;
+            }
+            if(pos == -1){
+                for(int i = cache.length - 1; i >= 1; i--){
+                    cache[i] = cache[i - 1];
+                }
+            }else{
+                for(int i = pos; i >= 1; i--){
+                    cache[i] = cache[i - 1];
+                }
+            }
+            cache[0] = num;
+        }
+
+        return cache;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int ea = in.nextInt();
+
+        int[] arr = new int[ea];
+        int[] arr2 = new int[ea];
+        for(int i = 0; i < ea; i++){
+            arr[i] = in.nextInt();
+            arr2[i] = arr[i];
+        }
+
+        for(int answer : solution1(size, ea, arr)) System.out.print(answer + " ");
+        for(int answer : solution2(size, ea, arr2)) System.out.print(answer + " ");
 
     }
 }
