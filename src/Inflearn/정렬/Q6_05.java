1c0affd5d2b3178c4e88941162b9f31ea65d17b1
lukejihwan <luke9701@naver.com>
2024-06-02 01:10:17 +0900

    다른 방식으로 문제 풀기

--- src/Inflearn/정렬/Q6_05.java
+++ src/Inflearn/정렬/Q6_05.java
@@ -1,29 +1,50 @@
 package Inflearn.정렬;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Q6_05 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        // Map으로 푸는 것이 좋을 것 같음
-        int N = in.nextInt();
-
-        int[] arr = new int[N];
+    private static String solution1(int[] arr){
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = in.nextInt();
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+
+        for(int key : arr){
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
         for(int check : arr){
             if(map.get(check) > 1){
-                System.out.println("D");
-                return;
+                return "D";
             }
         }
 
-        System.out.println("U");
+        return "U";
+    }
+
+    private static String solution2(int[] arr){
+
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] == arr[i + 1]) return "D";
+        }
+
+        return "U";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+        int[] arr2 = new int[N];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+            arr2[i] = arr[i];
+        }
+
+        System.out.println(solution1(arr));
+        System.out.println(solution2(arr2));
+
     }
 }
