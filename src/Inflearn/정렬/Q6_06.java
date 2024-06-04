ee44594dd369a15407d759f3bc0acd38304cb6ab
lukejihwan <luke9701@naver.com>
2024-06-04 22:54:16 +0900

    Q6_06 장난 꾸러기 문제 풀기

--- src/Inflearn/정렬/Q6_06.java
+++ src/Inflearn/정렬/Q6_06.java
@@ -1,17 +1,12 @@
 package Inflearn.정렬;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q6_06 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++){
-            arr[i] = in.nextInt();
-        }
-
+    private static ArrayList<Integer> solution1(int[] arr, int N){
+        ArrayList<Integer> list = new ArrayList<>();
         // 아래 케이스 외에 철수가 주변아이들과 키가 같은 경우도 존재하므로, 추가해야함
         boolean firstPrinted = false;
         for(int i = 0; i < N - 1; i++){
@@ -22,5 +17,34 @@ public class Q6_06 {
                 System.out.print(i + 2 + " ");
             }
         }
+        return list;
+    }
+
+    private static ArrayList<Integer> solution2(int[] arr, int N){
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        for(int i = 0; i < N; i++){
+            if(arr[i] != temp[i]){
+                answer.add(i + 1);
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for(int answer : solution2(arr, N)){
+            System.out.print(answer + " ");
+        }
+
     }
 }
