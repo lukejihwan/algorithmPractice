756290969b7daf9d3b02c98f0a9d46cbb9cf9e76
lukejihwan <luke9701@naver.com>
2024-06-07 23:52:10 +0900

    update

--- src/Inflearn/정렬/Q6_07.java
+++ src/Inflearn/정렬/Q6_07.java
@@ -6,5 +6,6 @@ public class Q6_07 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+        int[][] arr = new int[2][N];
     }
 }
