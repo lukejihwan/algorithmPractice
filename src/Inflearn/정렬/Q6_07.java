b939fcc49e0366fff0e8b31f27d4ffd10e21e390
lukejihwan <luke9701@naver.com>
2024-06-08 21:40:32 +0900

    좌표구하기 문제 풀기

--- src/Inflearn/정렬/Q6_07.java
+++ src/Inflearn/정렬/Q6_07.java
@@ -1,11 +1,42 @@
 package Inflearn.정렬;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Q6_07 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        int[][] arr = new int[2][N];
+
+        ArrayList<Pos> list = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            list.add(new Pos(in.nextInt(), in.nextInt()));
+        }
+
+        Collections.sort(list);
+
+        for(Pos o : list){
+            System.out.println(o.x + " " + o.y);
+        }
     }
+}
+
+class Pos implements Comparable<Pos>{
+    public int x, y;
+
+    Pos(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pos o){
+        if(this.x == o.x){
+            return o.y - this.y;
+        }else{
+            return this.x - o.x;
+        }
+    }
+
 }
