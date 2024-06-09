c4bcb4152ea4eb73efe071797bb861c396aaea05
lukejihwan <luke9701@naver.com>
2024-06-09 11:35:58 +0900

    Q6_08 이분 검색 문제 풀기

---
+++ src/Inflearn/정렬/Q6_08.java
@@ -0,0 +1,35 @@
+package Inflearn.정렬;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q6_08 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = N - 1;
+        while (true) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == K) {
+                System.out.println(mid + 1);
+                break;
+            }else if(arr[mid] < K){
+                start = mid;
+            }else if(arr[mid] > K){
+                end = mid;
+            }
+        }
+
+    }
+}
