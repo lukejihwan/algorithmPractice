f69cc98636cdf24229949c12a3c67da997596b3b
lukejihwan <luke9701@naver.com>
2024-06-11 23:41:44 +0900

    Q6_09 문제 풀기

---
+++ src/Inflearn/정렬/Q6_09.java
@@ -0,0 +1,11 @@
+package Inflearn.정렬;
+
+import java.util.Scanner;
+
+public class Q6_09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+    }
+}
