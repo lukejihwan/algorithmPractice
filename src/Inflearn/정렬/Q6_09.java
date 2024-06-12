d6b3fa6336f331081ef6fb030e2b623e99d281e6
lukejihwan <luke9701@naver.com>
2024-06-12 23:47:55 +0900

    결정 알고리즘 문제 풀기

--- src/Inflearn/정렬/Q6_09.java
+++ src/Inflearn/정렬/Q6_09.java
@@ -7,5 +7,10 @@ public class Q6_09 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
     }
 }
