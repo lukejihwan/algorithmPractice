7686e18cb1372518c58b5e1601be826d70c68ad8
lukejihwan <luke9701@naver.com>
2024-06-17 23:24:18 +0900

    Q6_10 문제 풀기

--- src/Inflearn/정렬/Q6_10.java
+++ src/Inflearn/정렬/Q6_10.java
@@ -1,9 +1,48 @@
 package Inflearn.정렬;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q6_10 {
+
+    private static int count(int[] arr, int mid){
+        int cnt = 1;
+        int ep = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] - ep >= mid){
+                cnt++;
+                ep = arr[i];
+            }
+        }
+
+        return cnt;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int lt = 1;
+        int rt = arr[N - 1];
+
+        int answer = 0;
+        while (lt <= rt){
+            int mid = (lt + rt) / 2;
+            if(count(arr, mid) >= K){
+                answer = mid;
+                rt = mid - 1;
+            }else{
+                lt = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
