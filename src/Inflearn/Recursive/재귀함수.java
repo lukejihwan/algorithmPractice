f4a1685a5c8ed8457a8c1895864a245a3c05fb6a
lukejihwan <111648451+lukejihwan@users.noreply.github.com>
2024-06-26 23:05:22 +0900

    재귀함수.java 업데이트

--- src/Inflearn/Recursive/재귀함수.java
+++ src/Inflearn/Recursive/재귀함수.java
@@ -14,6 +14,7 @@ public class 재귀함수 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+        int H = in.next();
         count(N);
     }
 }
