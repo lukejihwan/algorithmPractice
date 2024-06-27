3c2f62fac6c29f7149d9cef78f7f2b30f9c8d964
lukejihwan <luke9701@naver.com>
2024-06-27 19:36:35 +0900

    이진수 출력(재귀)

---
+++ src/Inflearn/Recursive/Q7_02.java
@@ -0,0 +1,26 @@
+package Inflearn.Recursive;
+
+import java.util.Scanner;
+
+public class Q7_02 {
+
+    private String str;
+    private static int DFS(int N){
+        //if()
+        //10 -> 1010
+        if(N % 2 == 0){
+
+        }else{
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        // 10진수 2진수로 출력하기
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        System.out.println(DFS(N));
+    }
+}
