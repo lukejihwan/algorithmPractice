bfee69519c954c1e168e1ee340a743044a89371f
lukejihwan <111648451+lukejihwan@users.noreply.github.com>
2024-06-29 23:49:11 +0900

    Q7_03.java 업데이트

--- src/Inflearn/Recursive/Q7_03.java
+++ src/Inflearn/Recursive/Q7_03.java
@@ -6,5 +6,6 @@ public class Q7_03 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+        int M = in.nextInt();
     }
 }
