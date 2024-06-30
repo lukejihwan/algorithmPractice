265ba5c4ef51a50d5add9e92a7a985fe759fb8ef
lukejihwan <luke9701@naver.com>
2024-06-30 11:14:16 +0900

    피보나치 재귀 함수 사용

--- src/Inflearn/Recursive/Q7_03.java
+++ src/Inflearn/Recursive/Q7_03.java
@@ -3,8 +3,16 @@ package Inflearn.Recursive;
 import java.util.Scanner;
 
 public class Q7_03 {
+    private static int fibonacci(int N){
+        if(N == 1) return 1;
+        else{
+            return N * fibonacci(N - 1);
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+
+        System.out.println(fibonacci(N));
     }
 }
