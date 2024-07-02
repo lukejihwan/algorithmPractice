3286f19ea1e55e50398042b83650926d3c7a48da
lukejihwan <luke9701@naver.com>
2024-07-02 23:53:54 +0900

    Q7_04

---
+++ src/Inflearn/Recursive/Q7_04.java
@@ -0,0 +1,9 @@
+package Inflearn.Recursive;
+
+import java.util.Scanner;
+
+public class Q7_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
