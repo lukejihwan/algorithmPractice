0c52fe10b8b5dd740712682f569afa0fb98efb92
lukejihwan <luke9701@naver.com>
2024-07-14 23:56:17 +0900

    새 클래스 작성

---
+++ src/Inflearn/Recursive/Q7_05.java
@@ -0,0 +1,10 @@
+package Inflearn.Recursive;
+
+import java.util.Scanner;
+
+public class Q7_05 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+    }
+}
