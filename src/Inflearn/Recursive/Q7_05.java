3e40661f90df5468573ee4cc59c63e6c14bf64a0
lukejihwan <luke9701@naver.com>
2024-07-20 23:06:11 +0900

    update

--- src/Inflearn/Recursive/Q7_05.java
+++ src/Inflearn/Recursive/Q7_05.java
@@ -3,6 +3,11 @@ package Inflearn.Recursive;
 import java.util.Scanner;
 
 public class Q7_05 {
+    private static int solution1(){
+
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
