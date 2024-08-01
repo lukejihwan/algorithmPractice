265bea15bf6fbc9d24f18751adc74959ec0e4031
lukejihwan <luke9701@naver.com>
2024-08-01 23:51:04 +0900

    부분집합구하기(DFS)

---
+++ src/Inflearn/DFS/Q7_06.java
@@ -0,0 +1,37 @@
+package Inflearn.DFS;
+
+import java.util.Scanner;
+
+public class Q7_06 {
+    static int n;
+    static int[] ch;
+
+    public static void DFS(int N){
+        if(N == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) tmp += (i + " ");
+            }
+            if (tmp.length() > 0) System.out.print(tmp);
+        }else{
+            ch[N] = 1;
+            DFS(N + 1);
+            ch[N] = 0;
+            DFS(N + 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        DFS(N);
+    }
+}
+
+class Nodes{
+    int data;
+
+
+
+}
