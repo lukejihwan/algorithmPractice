1eb0c31a961b71a94ed83bb72d325f837dc0f266
lukejihwan <luke9701@naver.com>
2024-08-08 00:02:29 +0900

    BFS 구현하기

--- src/Inflearn/BFS/Q7_08.java
+++ src/Inflearn/BFS/Q7_08.java
@@ -1,9 +1,27 @@
 package Inflearn.BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Q7_08 {
 
     private static void BFS(Node root){
 
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int level = 0;
+        while(!Q.isEmpty()){
+            int len = Q.size();
+            System.out.print(len + " : ");
+            for(int i = 0; i < len; i++){
+                Node pol = Q.poll();
+                System.out.print(pol.data + " ");
+                if(pol.lt != null) Q.offer(pol.lt);
+                if(pol.rt != null) Q.offer(pol.rt);
+            }
+            level++;
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         Node root = new Node(1);
