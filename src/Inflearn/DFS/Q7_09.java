2cef1af8bf887cb997bcbf646fc031136014f6b7
lukejihwan <luke9701@naver.com>
2024-08-18 23:59:14 +0900

    문제

--- src/Inflearn/DFS/Q7_09.java
+++ src/Inflearn/DFS/Q7_09.java
@@ -1,7 +1,23 @@
 package Inflearn.DFS;
 
 public class Q7_09 {
+    Node root;
     public static void main(String[] args) {
+        Q7_09 tree = new Q7_09();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.DFS(0, tree.root));
 
+    }
+
+    private int DFS(int L, Node root){
+        if(root.lt == null && root.rt == null){
+            return L;
+        }else{
+            return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+        }
     }
 }
