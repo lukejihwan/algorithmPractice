3eb6170b2ba314d90d27331221e98f746d956f06
lukejihwan <luke9701@naver.com>
2024-08-22 23:43:05 +0900

    update

---
+++ src/Inflearn/AdjacencyList/Q7_14.java
@@ -0,0 +1,11 @@
+package Inflearn.AdjacencyList;
+
+import java.util.Scanner;
+
+public class Q7_14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+    }
+}
