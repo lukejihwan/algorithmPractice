28674089a879c5611e16a578aa4d2589b030c258
lukejihwan <luke9701@naver.com>
2024-08-26 00:59:22 +0900

    update

--- src/Inflearn/DFS/Q8_01.java
+++ src/Inflearn/DFS/Q8_01.java
@@ -3,7 +3,31 @@ package Inflearn.DFS;
 import java.util.Scanner;
 
 public class Q8_01 {
+    static int n, total;
+    static String answer = "NO";
+    static int[] arr;
+
+    public static String DFS(int L, int sum, boolean flag){
+        if(n == L){
+            if((total - sum) == sum){
+                return answer = "YES";
+            }
+        }else{
+            DFS(L + 1, sum + arr[L], true);
+            DFS(L + 1, sum + arr[L], false);
+        }
+
+    }
     public static void main(String[] args) {
+        Q8_01 main = new Q8_01();
         Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        arr = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            arr[i] = in.nextInt();
+            total += arr[i];
+        }
+
+        DFS(0, 0, true);
     }
 }
