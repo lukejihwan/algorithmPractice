b0d5f48321959abd667d2319232e182c9d6cfbd2
lukejihwan <luke9701@naver.com>
2024-08-29 23:53:21 +0900

    최대점수 구하기 문제 풀기 세팅

---
+++ src/Inflearn/DFS/Q8_03.java
@@ -0,0 +1,14 @@
+package Inflearn.DFS;
+
+import java.util.Scanner;
+
+public class Q8_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+        }
+    }
+}
