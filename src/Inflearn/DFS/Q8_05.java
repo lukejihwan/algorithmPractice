18f51e23fb1ea1092ab390ea7cb63560b9213d5d
lukejihwan <luke9701@naver.com>
2024-09-05 23:53:28 +0900

    update

--- src/Inflearn/DFS/Q8_05.java
+++ src/Inflearn/DFS/Q8_05.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Q8_05 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
     }
 }
