b209eb7c31f6f5cf37543dc22e236bdf683971b8
lukejihwan <luke9701@naver.com>
2024-09-08 22:07:36 +0900

    동전교환 문제 풀기

--- src/Inflearn/DFS/Q8_05.java
+++ src/Inflearn/DFS/Q8_05.java
@@ -3,14 +3,33 @@ package Inflearn.DFS;
 import java.util.Scanner;
 
 public class Q8_05 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        int Z = in.nextInt();
+    static int n, m, ea;
+    static int answer = 9999999;
+    static int[] coins;
 
-        for(int i = 0; i < N; i++){
-
+    private void DFS(int sum, int ea){
+        if(sum == m){
+            if(ea < answer) answer = ea;
+        }else{
+            for(int i = 0; i < n; i++){
+                DFS(sum + coins[i], ea + 1);
+            }
         }
+
+    }
+
+    public static void main(String[] args){
+        Q8_05 main = new Q8_05();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        coins = new int[n];
+        for(int i = 0; i < n; i++){
+            coins[i] = in.nextInt();
+        }
+        m = in.nextInt();
+        int sum = 0;
+        main.DFS(sum, 0);
+        System.out.println(answer);
     }
 }
