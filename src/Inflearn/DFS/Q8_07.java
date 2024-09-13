1466b85031a30f4bc886f69e0a1c1d587c047bba
lukejihwan <luke9701@naver.com>
2024-09-13 23:50:53 +0900

    조합의 경우수(메모이제이션) 문제 풀기

--- src/Inflearn/DFS/Q8_07.java
+++ src/Inflearn/DFS/Q8_07.java
@@ -3,9 +3,15 @@ package Inflearn.DFS;
 import java.util.Scanner;
 
 public class Q8_07 {
+
+    private void DFS(int n, int m){
+
+    }
     public static void main(String[] args) {
+        Q8_07 main = new Q8_07();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
+        main.DFS(n, m);
     }
 }
