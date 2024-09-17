21fc9ec5c14f6de82ec495038cf9da9d943c6574
lukejihwan <luke9701@naver.com>
2024-09-17 23:44:22 +0900

    조합의 경우의 수 찾기(메모이제이션)

--- src/Inflearn/DFS/Q8_07.java
+++ src/Inflearn/DFS/Q8_07.java
@@ -3,9 +3,16 @@ package Inflearn.DFS;
 import java.util.Scanner;
 
 public class Q8_07 {
+    static int answer;
 
     private void DFS(int n, int m){
-        n * m;
+        if(m == 0 || n == m){
+            answer++;
+            return;
+        }else{
+            DFS(n - 1, m - 1);
+            DFS(n - 1, m);
+        }
     }
     public static void main(String[] args) {
         Q8_07 main = new Q8_07();
@@ -13,6 +20,6 @@ public class Q8_07 {
         int n = in.nextInt();
         int m = in.nextInt();
         main.DFS(n, m);
-        System.out.println();
+        System.out.println(answer);
     }
 }
