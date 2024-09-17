d49ca7af5dcf070bb3e07c77ea781667a56ab79f
lukejihwan <luke9701@naver.com>
2024-09-17 23:52:00 +0900

    좀더 빠른 방식으로 수정

--- src/Inflearn/DFS/Q8_07.java
+++ src/Inflearn/DFS/Q8_07.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Q8_07 {
     static int answer;
+    static int[][] hint;
 
     private void DFS(int n, int m){
+        if(hint[n][m] != 0){
+            answer += hint[n][m];
+            return;
+        }
         if(m == 0 || n == m){
             answer++;
             return;
@@ -19,6 +24,7 @@ public class Q8_07 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
+        hint = new int[n + 1][m + 1];
         main.DFS(n, m);
         System.out.println(answer);
     }
