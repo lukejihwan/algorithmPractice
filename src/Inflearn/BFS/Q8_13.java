9779943f587edc0663338694e8462901fab4b0fa
lukejihwan <luke9701@naver.com>
2024-09-24 00:04:44 +0900

    update

--- src/Inflearn/BFS/Q8_13.java
+++ src/Inflearn/BFS/Q8_13.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class Q8_13 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Q8_13 main = new Q8_13();
     }
 }
