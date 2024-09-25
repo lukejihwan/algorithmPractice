c5c5cc6d20d915bd131e03fb9334348c62f3e3e0
lukejihwan <luke9701@naver.com>
2024-09-25 23:34:08 +0900

    섬 갯수 구하기

--- src/Inflearn/BFS/Q8_13.java
+++ src/Inflearn/BFS/Q8_13.java
@@ -6,5 +6,13 @@ public class Q8_13 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Q8_13 main = new Q8_13();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[][] board = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; i++){
+                board[m][n] = in.nextInt();
+            }
+        }
     }
 }
