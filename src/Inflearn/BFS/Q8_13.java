7dbe218ee793814318b8a49fdbe5a53b793518b9
lukejihwan <luke9701@naver.com>
2024-10-02 21:56:00 +0900

    섬나라 아일랜드 문제풀기

--- src/Inflearn/BFS/Q8_13.java
+++ src/Inflearn/BFS/Q8_13.java
@@ -3,24 +3,47 @@ package Inflearn.BFS;
 import java.util.Scanner;
 
 public class Q8_13 {
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
+    static int answer, m;
 
-    private void BFS(int m, int n){
-        Queue Q = new LinkedList<>();
-        Q.poll();
-        for(int i=0; i < m; i++){}
+    private void DFS(int x, int y, int[][] board){
+        for(int i = 0; i < 8; i++){
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if(nextX >= 0 && nextX < m && nextY >= 0 && nextY < m && board[nextX][nextY] == 1){
+                board[nextX][nextY] = 0;
+                DFS(nextX, nextY, board);
+            }
+        }
+
+    }
+    private void solution(int[][] board){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < m; j++){
+                int num = board[i][j];
+                if(num == 1){
+                    answer++;
+                    board[i][j] = 0;
+                    DFS(i, j, board);
+                }
+            }
+
+        }
 
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Q8_13 main = new Q8_13();
-        int m = in.nextInt();
-        int n = in.nextInt();
-        int[][] board = new int[m][n];
+        m = in.nextInt();
+        int[][] board = new int[m][m];
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; i++){
-                board[m][n] = in.nextInt();
+            for(int j = 0; j < m; j++){
+                board[i][j] = in.nextInt();
             }
         }
-        main.BFS();
+        main.solution(board);
+        System.out.println(answer);
+
     }
 }
