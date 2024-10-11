af3f929f4c863cb8b8623c4c5ff3c91816d810f7
lukejihwan <111648451+lukejihwan@users.noreply.github.com>
2024-10-11 23:52:56 +0900

    Q8_15.java 업데이트

--- src/Inflearn/DFS/Q8_15.java
+++ src/Inflearn/DFS/Q8_15.java
@@ -3,7 +3,7 @@ package Inflearn.DFS;
 import java.util.Scanner;
 
 public class Q8_15 {
-    int j;
+    int j, i;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
     }
