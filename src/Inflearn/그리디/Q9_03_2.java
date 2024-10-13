768f7db7a7c91187fcd0888c2bfa885dcd2d95c2
lukejihwan <luke9701@naver.com>
2024-10-14 00:22:57 +0900

    결혼식 문제풀기

--- src/Inflearn/그리디/Q9_03_2.java
+++ src/Inflearn/그리디/Q9_03_2.java
@@ -1,7 +1,42 @@
 package Inflearn.그리디;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Q9_03_2 {
+
+    private int solution(ArrayList<Point3_2> arr){
+
+        int cnt = 0;
+        int max = 0;
+        for(Point3_2 one : arr){
+            if(one.state == 's'){
+                cnt++;
+            }else{
+                cnt--;
+            }
+            max = Math.max(cnt, max);
+        }
+
+        return max;
+    }
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Q9_03_2 main = new Q9_03_2();
+        int n = in.nextInt();
+
+        ArrayList<Point3_2> arr = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            int f = in.nextInt();
+            arr.add(new Point3_2(f, 's'));
+            int e = in.nextInt();
+            arr.add(new Point3_2(e, 'e'));
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(main.solution(arr));
 
     }
 }
