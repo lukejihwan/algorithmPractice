6bd18a0eef543271d6d3ac6a15b38135a663b56f
lukejihwan <luke9701@naver.com>
2024-10-15 23:54:37 +0900

    최대수입 스케쥴 문제 풀기

--- src/Inflearn/그리디/Q9_04.java
+++ src/Inflearn/그리디/Q9_04.java
@@ -1,12 +1,38 @@
 package Inflearn.그리디;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Q9_04 {
+
+    private int greed(ArrayList<Point4> list){
+        PriorityQueue<Integer> pQ = new PriorityQueue(Collections.reverseOrder());
+
+        for(Point4 a : list){
+            System.out.println(a.m + " " + a.d);
+        }
+
+
+
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Q9_04 main = new Q9_04();
         int n = in.nextInt();
+        ArrayList<Point4> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            int m = in.nextInt();
+            int d = in.nextInt();
+            list.add(new Point4(m, d));
+        }
 
+        Collections.sort(list);
+
+        main.greed(list);
 
     }
 }
