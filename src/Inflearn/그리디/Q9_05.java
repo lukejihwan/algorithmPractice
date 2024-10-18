c98764afe7a95e6f23166e5b2e681c72c4116180
lukejihwan <111648451+lukejihwan@users.noreply.github.com>
2024-10-18 23:28:39 +0900

    Q9_05.java 업데이트

--- src/Inflearn/그리디/Q9_05.java
+++ src/Inflearn/그리디/Q9_05.java
@@ -3,7 +3,7 @@ package Inflearn.그리디;
 import java.util.Scanner;
 
 public class Q9_05 {
-    static int m;
+    static int m, n;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
