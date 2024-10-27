716aa5593eb6fcf91b140ca44bd1524e7dde548d
lukejihwan <luke9701@naver.com>
2024-10-27 23:46:22 +0900

    다익스트라 알고리즘 문제 풀기

--- src/Inflearn/그리디/Q9_05.java
+++ src/Inflearn/그리디/Q9_05.java
@@ -1,9 +1,18 @@
 package Inflearn.그리디;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Q9_05 {
-    static int m, n, x;
+    static int m, n;
+    static ArrayList<ArrayList<Edge>> graph;
+    static int[] dis;
+    public void solution(int v){
+        PriorityQueue<Edge> pQ = new PriorityQueue<Edge>();
+
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
