5963550d677e1254acae41a935511558d42c5bcb
lukejihwan <luke9701@naver.com>
2024-10-28 23:51:29 +0900

    다익스트라 알고리즘 문제 풀기

--- src/Inflearn/그리디/Q9_05.java
+++ src/Inflearn/그리디/Q9_05.java
@@ -2,6 +2,7 @@ package Inflearn.그리디;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -11,12 +12,56 @@ public class Q9_05 {
     static int[] dis;
     public void solution(int v){
         PriorityQueue<Edge> pQ = new PriorityQueue<Edge>();
+        pQ.offer(new Edge(v, 0));
+        dis[v] = 0;
+        while(!pQ.isEmpty()){
+            Edge tmp = pQ.poll();
+            int now = tmp.vex;
+            int nowCost = tmp.cost;
+            if(nowCost > dis[now]) continue;
+            for(Edge ob : graph.get(now)){
+                if(dis[ob.vex] > nowCost + ob.cost){
+                    dis[ob.vex] = nowCost + ob.cost;
+                    pQ.offer(new Edge(ob.vex, nowCost + ob.cost));
+                }
+            }
+        }
 
     }
     public static void main(String[] args) {
+        Q9_05 main = new Q9_05();
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] arr = new int[n];
+        n = in.nextInt();
+        m = in.nextInt();
+        graph = new ArrayList<ArrayList<Edge>>();
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<Edge>());
+        }
+        dis = new int[n + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for(int i = 0; i < m; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            graph.get(a).add(new Edge(b, c));
+        }
+        main.solution(1);
+        for(int i = 2; i <= n; i++){
+            if(dis[i] != Integer.MAX_VALUE) System.out.println(i + " " + dis[i]);
+            else System.out.println(i + " : impossible");
+        }
+    }
+}
 
+class Edge implements Comparable<Edge>{
+    int vex;
+    int cost;
+    Edge(int vex, int cost){
+        this.vex = vex;
+        this.cost = cost;
+    }
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
     }
 }
