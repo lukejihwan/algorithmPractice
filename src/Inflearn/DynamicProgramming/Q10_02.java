7e1e6fdab0465d0f946cafffdde46ed47c8de628
lukejihwan <luke9701@naver.com>
2024-11-13 23:48:18 +0900

    돌다리 건너기 문제 풀기

---
+++ src/Inflearn/DynamicProgramming/Q10_02.java
@@ -0,0 +1,27 @@
+package Inflearn.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class Q10_02 {
+    static int[] arr;
+
+    private int solution(int n){
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i = 3; i <= n + 1; i++){
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n + 1];
+    }
+
+    public static void main(String[] args) {
+        Q10_02 main = new Q10_02();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        arr = new int[n + 2];
+
+        System.out.println(main.solution(n));
+    }
+}
