098746117ebeed01ef7b5fd0c97cc36272df7cbb
lukejihwan <luke9701@naver.com>
2024-11-17 23:58:12 +0900

    update

---
+++ src/Inflearn/DynamicProgramming/Q10_03.java
@@ -0,0 +1,18 @@
+package Inflearn.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class Q10_03 {
+    static int d;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+
+
+    }
+}
