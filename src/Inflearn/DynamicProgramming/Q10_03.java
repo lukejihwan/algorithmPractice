f5063b2ebfedfa49114c8864201c9cb304229993
lukejihwan <luke9701@naver.com>
2024-11-21 23:31:40 +0900

    메서드 추가

--- src/Inflearn/DynamicProgramming/Q10_03.java
+++ src/Inflearn/DynamicProgramming/Q10_03.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Q10_03 {
     static int d;
     static float[] f;
+
+    private static int solution(int[] arr){
+
+
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -13,6 +19,8 @@ public class Q10_03 {
         for(int i = 0; i < n; i++){
             arr[i] = in.nextInt();
         }
+
+        System.out.println(solution(arr));
 
 
     }
