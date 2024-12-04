a99272ac5ea5cc4d6c5bda46a73ad05ca6b2a773
lukejihwan <luke9701@naver.com>
2024-12-04 23:44:58 +0900

    최대 부분 증가 수열 문제 풀기

--- src/Inflearn/DynamicProgramming/Q10_03.java
+++ src/Inflearn/DynamicProgramming/Q10_03.java
@@ -6,32 +6,35 @@ import java.util.Scanner;
 
 public class Q10_03 {
     static int d;
+    static int[] dy;
     static float[] f;
     static boolean flag;
     static boolean j;
 
     private static int solution(int[] arr){
-
-
-        return 0;
+        int answer = 0;
+        dy = new int[arr.length];
+        dy[0] = 1;
+        for(int i = 1; i < arr.length; i++){
+            int max = 0;
+            for(int j = i - 1; j >= 0; j--){
+                if(arr[i] < arr[i] && dy[j] > max) max = dy[j];
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
     }
 
 
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            int[] arr = new int[n];
-            int a;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
 
-            for(int i = 0; i < n; i++){
-                arr[i] = in.nextInt();
-            }
-
-        int[] array = new int[n + 1];
-        for(int i = 1; i < array.length; i++){
-            array[i] = in.nextInt();
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
         }
-
 
         System.out.println(solution(arr));
 
