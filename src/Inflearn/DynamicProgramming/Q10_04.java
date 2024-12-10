b1a8971b47055042e00d535a7f7c2d6c54a3753b
lukejihwan <luke9701@naver.com>
2024-12-10 23:56:43 +0900

    새문제 생성

---
+++ src/Inflearn/DynamicProgramming/Q10_04.java
@@ -0,0 +1,10 @@
+package Inflearn.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class Q10_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+    }
+}
