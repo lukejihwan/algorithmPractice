01362de04723f638a5957277fc028c746a421300
lukejihwan <luke9701@naver.com>
2024-12-19 22:28:49 +0900

    Q1759 문제 풀기

---
+++ src/BaekJoon/Q1759.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Q1759 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        int c = in.nextInt();
+    }
+}
