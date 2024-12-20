bb2914b61fc93433a6fdcb823000354feb790359
lukejihwan <luke9701@naver.com>
2024-12-21 01:05:05 +0900

    암호만들기 문제 풀기

--- src/BaekJoon/Q1759.java
+++ src/BaekJoon/Q1759.java
@@ -1,19 +1,70 @@
 package BaekJoon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Q1759 {
+    static ArrayList<String> list = new ArrayList<>();
+    static int l, c;
+    static String[] brute, answer;
+    static String[] vowels = {"a", "e", "i", "o", "u"};
+
+    public static void solution(){
+        // 모음 1개에 자음 2개이상이어야 함
+        // level 4 까지임
+        // 4개가 완성되고 나서 모음 1, 자음 2를 체크해야함
+
+        // i까지 반복한다.
+        int i = 0;
+        while(i != l){
+            DFS(brute[i], 1);
+            i++;
+        }
+
+    }
+
+    public static void DFS(String element, int level){
+        if(level == l){
+            int vowelEa = 0;
+            for(int i = 0; i < l; i++){
+                for(int j = 0; j < vowels.length; j++){
+                    if(answer[i] == vowels[j]) vowelEa++;
+                }
+            }
+            if(vowelEa >= 1 && (l - vowelEa) >= 2){
+                String g = "";
+                for(int i = 0; i < l; i++){
+                    g += answer[i];
+                }
+                list.add(g);
+            }
+            return;
+        }else{
+            DFS(element, level++);
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int l = in.nextInt();
-        int c = in.nextInt();
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        String[] brute = new String[c];
+        l = in.nextInt();
+        c = in.nextInt();
+        brute = new String[c];
+        answer = new String[l];
         for(int i = 0; i < c; i++){
             brute[i] = in.next();
         }
 
+        Arrays.sort(brute);
+
         //백트래킹을 활용한 브루트 포스 문제
+        solution();
+
+        for(String ans : list){
+            System.out.println(ans);
+        }
 
     }
 }
---
+++ src/BaekJoon/Q1759_02.java
@@ -0,0 +1,53 @@
+package BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1759_02 {
+    static char[] arr;
+    static boolean[] visited;
+    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    static int l, c;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        l = in.nextInt();
+        c = in.nextInt();
+        arr = new char[c];
+        visited = new boolean[c];
+
+        for(int i = 0; i < c; i++){
+            arr[i] = in.next().charAt(0);
+        }
+
+        Arrays.sort(arr);
+
+        DFS(0, 0);
+
+    }
+
+    public static void DFS(int start, int level){
+        if(level == l){
+            String answer = "";
+            for(int i = 0; i < c; i++){
+                if(visited[i]) answer += arr[i];
+            }
+
+            int vowelEa = 0;
+            for(int i = 0; i < l; i++){
+                for(int j = 0; j < vowels.length; j++){
+                    if(answer.charAt(i) == vowels[j]) vowelEa++;
+                }
+            }
+            if(vowelEa >= 1 && (l - vowelEa) >= 2){
+                System.out.println(answer);
+            }
+        }
+
+        for(int i = start; i < c; i++){
+            visited[i] = true;
+            DFS(i + 1, level + 1);
+            visited[i] = false;
+        }
+
+    }
+}
