3ce66eef4f3ed04a2a947d67d086ab5015af6ad3
lukejihwan <luke9701@naver.com>
2024-12-20 23:57:36 +0900

    update

--- src/BaekJoon/Q1759.java
+++ src/BaekJoon/Q1759.java
@@ -7,5 +7,13 @@ public class Q1759 {
         Scanner in = new Scanner(System.in);
         int l = in.nextInt();
         int c = in.nextInt();
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        String[] brute = new String[c];
+        for(int i = 0; i < c; i++){
+            brute[i] = in.next();
+        }
+
+        //백트래킹을 활용한 브루트 포스 문제
+
     }
 }
