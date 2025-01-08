96003460d0b8a13bb9cdf3774b03edf466ee3e7f
lukejihwan <luke9701@naver.com>
2025-01-08 23:49:45 +0900

    DSLR문제 풀기

---
+++ src/BaekJoon/Q9019.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Q9019 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] numbers = new int[n][2];
+        for(int i = 0; i < n; i++){
+            numbers[n][0] = in.nextInt();
+            numbers[n][1] = in.nextInt();
+        }
+
+
+    }
+}
