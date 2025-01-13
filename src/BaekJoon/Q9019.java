89a9ddedcd9d6ebb1407c9de325418109e66a395
lukejihwan <luke9701@naver.com>
2025-01-13 23:05:40 +0900

    Q9019 DSLR문제 풀기

--- src/BaekJoon/Q9019.java
+++ src/BaekJoon/Q9019.java
@@ -1,20 +1,68 @@
 package BaekJoon;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Q9019 {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[][] numbers = new int[n][2];
         for(int i = 0; i < n; i++){
-            numbers[n][0] = in.nextInt();
-            numbers[n][1] = in.nextInt();
+            numbers[i][0] = in.nextInt();
+            numbers[i][1] = in.nextInt();
         }
 
-        int m = in.nextInt();
-        int x = in.nextInt();
-        int z = in.nextInt();
+        for(int i = 0; i < n; i++){
+            int init = numbers[i][0];
+            int goal = numbers[i][1];
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] visited = new boolean[10000];
+            String[] command = new String[10000];
+
+            q.add(init);
+            visited[init] = true;
+            Arrays.fill(command, "");
+
+            while(!q.isEmpty() && !visited[goal]){
+                int now = q.poll();
+
+                int D = (2 * now) % 10000;
+                int S = now == 0 ? 9999 : now - 1;
+                int L = (now % 1000) * 10 + now / 1000;
+                int R = (now % 10) * 1000 + now / 10;
+
+                if (!visited[D]) {
+                    q.add(D);
+                    visited[D] = true;
+                    command[D] = command[now] + "D";
+                }
+
+                if (!visited[S]) {
+                    q.add(S);
+                    visited[S] = true;
+                    command[S] = command[now] + "S";
+                }
+
+                if (!visited[L]) {
+                    q.add(L);
+                    visited[L] = true;
+                    command[L] = command[now] + "L";
+                }
+
+                if (!visited[R]) {
+                    q.add(R);
+                    visited[R] = true;
+                    command[R] = command[now] + "R";
+                }
+            }
+            System.out.println(command[goal]);
+
+        }
+
 
 
     }
