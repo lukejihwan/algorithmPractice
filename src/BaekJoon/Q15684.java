ed1f5a7e0faf6324a75626aba20b7ae1a3520ae1
lukejihwan <luke9701@naver.com>
2025-01-21 21:47:16 +0900

    사다리타기 문제풀기

--- src/BaekJoon/Q15684.java
+++ src/BaekJoon/Q15684.java
@@ -1,12 +1,68 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Q15684 {
-    public static void main(String[] args) {
+    private static int n, m, h, answer = 4;
+    private static int[][] map;
+    public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    h = Integer.parseInt(st.nextToken());
+
+    map = new int[h + 1][n + 1];
+
+    int x, y;
+        for (int i = 0; i < m; i++) {
+        st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        map[x][y] = 1;
+        map[x][y + 1] = 2;
+    }
+
+    dfs(1, 0);
+        System.out.println((answer != 4) ? answer : -1);
+}
+
+    private static void dfs(int x, int count) {
+        if (answer <= count) return;
+        else {
+            if (check()) {
+                answer = count;
+                return;
+            }
+        }
+
+        for (int i = x; i < h + 1; i++) {
+            for (int j = 1; j < n; j++) {
+                if (map[i][j] == 0 && map[i][j + 1] == 0) {
+                    map[i][j] = 1;
+                    map[i][j + 1] = 2;
+                    dfs(i, count + 1);
+                    map[i][j] = map[i][j + 1] = 0;
+                }
+            }
+        }
+    }
+
+    private static boolean check() {
+        for (int i = 1; i <= n; i++) {
+            int x = 1, y = i;
+            for (int j = 0; j < h; j++) {
+                if (map[x][y] == 1) y++;
+                else if (map[x][y] == 2) y--;
+                x++;
+            }
+            if (y != i) return false;
+        }
+        return true;
     }
 }
