b6f433402f1cc3229815032babaa57037d0732d0
lukejihwan <luke9701@naver.com>
2025-02-07 23:45:08 +0900

    update

---
+++ src/BaekJoon/Q15684_2.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Q15684_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+    }
+}
