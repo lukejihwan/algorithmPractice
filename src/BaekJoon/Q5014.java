9161fb93107b62e969f2b37b776de8b48c298ad6
lukejihwan <luke9701@naver.com>
2025-03-08 23:27:18 +0900

    update

--- src/BaekJoon/Q5014.java
+++ src/BaekJoon/Q5014.java
@@ -57,4 +57,39 @@ public class Q5014 {
 
         System.out.println("use the stairs");
     }
+
+    static void DFS(){
+        Queue<Integer> q = new LinkedList<>();
+        int[] move = new int[floor + 1];
+        // 방문한 곳 체크하기 위한 boolean배열
+        boolean[] visited = new boolean[floor + 1];
+
+        q.add(init);
+        visited[init] = true;
+        move[init] = 0;
+
+        while(!q.isEmpty()){
+            int num = q.poll();
+
+            if(num == goal){
+                System.out.println(move[num]);
+                return;
+            }
+
+            for(int i=0; i<2; i++) {
+                int next = num + dir[i];
+
+                if(next > floor || next < 1 ) continue;
+
+                if(!visited[next]) {
+                    visited[next] = true;
+                    q.add(next);
+                    move[next]= move[num] + 1;
+                }
+            }
+
+        }
+
+        System.out.println("use the elevator");
+    }
 }
